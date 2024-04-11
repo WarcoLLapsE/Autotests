40197c1aec01db4e69ee266957b7317628a9c18d
WarcoLLapsE <warcollapse@gmail.com>
2024-04-11 22:10:10 +0300

    new tests with fixtures

--- Block_1/test2.py
+++ Block_1/test2.py
@@ -1,1 +1,20 @@
-print(10 * 6)
+from string import ascii_lowercase
+from string import digits
+
+
+nick = input()
+flag = True
+if not nick.startswith('@'):
+    flag = False
+if len(nick) < 5 or len(nick) > 15:
+    flag = False
+for i in nick[1:]:
+    if i in ascii_lowercase or i in digits:
+        continue
+    else:
+        flag = False
+        break
+if flag:
+    print('Correct')
+else:
+    print('Incorrect')
