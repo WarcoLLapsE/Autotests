d9d31a8da001e96e0470390c2903a35b8e5f98b0
WarcoLLapsE <warcollapse@gmail.com>
2024-12-10 22:06:09 +0300

    3 block is done

--- Block_1/test2.py
+++ Block_1/test2.py
@@ -1,5 +1,21 @@
-import math
-import time
+import turtle
 
-answer = math.log(int(time.time()))
-print(answer)
+
+def hexagon(side):
+    turtle.shape('turtle')
+    angle = 0
+    for _ in range(6):
+        turtle.setheading(angle)
+        turtle.forward(side)
+        angle += 60
+
+
+# angle_honeycomb = 0
+# while angle_honeycomb != 360:
+#     hexagon(80)
+#     turtle.heading(angle_honeycomb)
+#     angle_honeycomb += 30
+
+hexagon(80)
+turtle.forward(160)
+hexagon(80)
